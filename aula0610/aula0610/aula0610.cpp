a7c16ebbea631f4f1e84cc984bc763982fa6deaf
luizgatao <luizeduardorigoni@hotmail.com>
2017-10-07 14:04:33 -0300

    projeto aula0610

--- aula0610/aula0610/aula0610.cpp
+++ aula0610/aula0610/aula0610.cpp
@@ -8,38 +8,52 @@
 #include "locale.h"
 #include <conio.h>
 
-int numero1, numero2;
-float resultado;
+int numero1, numero2, resultado2;
+float resultado, resultado3;
 
 int main()
 {
 	setlocale(LC_ALL, "");
-	system("color 0");
+	system("color A");
 	printf("digite oprimeiro numero.\n");
 	scanf_s("%i", &numero1);
+	system("color B");
 	printf("digite o segundo numero\n");
 	scanf_s("%i", &numero2);
 
 
-
-	printf("========================\n");
-	resultado = numero1 + numero2;
-	printf("a soma e:%i\n", resultado);
-	printf("========================\n");
-	resultado = numero1 - numero2;
-	printf("a subtraçao e:%i\n", resultado);
-	printf("========================\n");
-	resultado = numero1 * numero2;
-	printf("multiplicacao e:%i\n", resultado);
-	printf("========================\n");
-	resultado = (float)numero1 / numero2;
-	printf("sua divisao e:%f\n", resultado);
-	printf("========================\n");
+	system("color F");
+	printf("\n\n\n\n\n\n\n========================\n");
+	resultado2 = numero1 + numero2;
+	system("color C");
+	printf("a soma e:%i\n", resultado2);
+	system("color F");
+	printf("========================\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	system("pause");
+	printf("\n\n\n\n\n\n\n========================\n");
+	system("color 3");
+	printf("a subtraçao e:%i\n", resultado = numero1 - numero2);
+	system("color F");
+	printf("========================\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	system("pause");
+	printf("\n\n\n\n\n\n\n========================\n");
+	resultado2 = numero1 * numero2;
+	system("color C");
+	printf("multiplicacao e:%i\n", resultado2);
+	system("color F");
+	printf("========================\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	system("pause");
+	printf("\n\n\n\n\n\n\n========================\n");
+	system("color 3");
+	printf("sua divisao e:%f\n", resultado3 =(float) numero1 / numero2);
+	system("color F");
+	printf("========================\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	system("pause");
+	printf("\n\n\n\n\n\n\n========================\n");
+	system("color C");
 	printf("o resto da divisao e %i\n", numero1 = numero1 % numero2);
-	printf("========================\n");
-
-
-
+	system("color F");
+	printf("========================\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 	system("pause");
     return 0;
 }
